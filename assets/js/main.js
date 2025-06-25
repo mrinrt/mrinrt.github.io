a6b3c1e609b1716a5877c4625e9478452820805e
nguyenxx <nguyenxx@users.noreply.github.com>
2025-06-25 04:35:27 -0400

    update website for transfer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -93,6 +93,18 @@
     }
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
+
+    // added this block to apply same topmenu behavior for archive:
+    window.addEventListener('load', () => {
+      const pathname = window.location.pathname
+      if (pathname === '/archive/' || pathname === '/archive/index.html') {
+        requestAnimationFrame(() => {
+          if (window.scrollY <= 100) {
+            selectHeader.classList.remove('header-scrolled')
+          }
+        })
+      }
+    })
   }
 
   /**
@@ -114,7 +126,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -123,7 +135,7 @@
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
+  on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
@@ -133,7 +145,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
